7d9a52878788562981035b57019d078725bcab4b
Vinod <dhyawanavinod2020@gmail.com>
2022-01-01 22:50:17 +0530

    Some css added to form

--- js/form.js
+++ js/form.js
@@ -1,26 +1,71 @@
-var i;
+let i;
+let j;
 function call() {
-  var teamMember = document.getElementById("hello");
+  // var teamMember = document.getElementById(`hello${1}`);
 
-  for (var a = 2; a <= i; a++) {
-    teamMember.innerHTML += "<h3>Team Member " + a + " </h3> <label for='teamCaptain' class='form-label'>Name</label> <input class='form-control' type='text' name='captainName' value='' placeholder='Enter Captain Name'> <label for='teamCaptain' class='form-label'>Roll Number</label> <input class='form-control' type='text' name='captainRollno' value='' placeholder='Enter Roll Number'> <label for='teamCaptain' class='form-label'>Email</label><input class='form-control' type='email' name='captainEmail' value='' placeholder='Enter Email'>";
-
-  }
+  // for (let a = 2; a <= i;a++) {
+  //   teamMember.innerHTML = "<h3> Player " + a + " </h3><input class='form-control' type='text' name='player"+a+"Name' value='' placeholder='Enter Player Name'> <input class='form-control' type='text' name='player"+a+"Rollno' value='' placeholder='Enter Roll Number'><input class='form-control' type='email' name='player"+a+"Email' value='' placeholder='Enter Email'>\n <input type='button' class='nxtbtn' id='next' onclick='whenClick0()' ; value='Next'>";
+  //   //  if(whenClick0);
+  //   //  else 
+  //   //  {
+  //   //    a = a-1;
+  //   //    console.log(whenClick0);
+  //   //  }
+  //   console.log(whenClick0);
+  // }
 }
 
 function whenClick1() {
   i = 7;
+  j = 1 ;
   call();
   document.getElementById("kabbadi").remove();
   document.getElementById("khokho").remove();
   document.getElementById("satoliya").remove();
   document.getElementById("gameLabel").remove();
+  document.getElementById("mainhead").remove();
+  document.getElementById("teamName").remove();
+  var cl = document.getElementById(`page1`);
+  cl.style.display = "block";
 }
 function whenClick2() {
   i = 9;
+  j = 1 ;
   call();
   document.getElementById("kabbadi").remove();
   document.getElementById("khokho").remove();
   document.getElementById("satoliya").remove();
   document.getElementById("gameLabel").remove();
+  document.getElementById("mainhead").remove();
+  document.getElementById("teamName").remove();
+  var cl = document.getElementById(`page1`);
+  cl.style.display = "block";
 }
+function whenClicknext()
+{
+    var curr = document.getElementById(`page${j}`);
+    curr.style.display = "none";
+    j += 1;
+    if( j < i )
+    {
+      var nxt = document.getElementById(`page${j}`);
+      nxt.style.display = "block";
+    }
+    if( j == i )
+    {
+      var last = document.getElementById(`page${i}`);
+      last.innerHTML = "<h3 class = 'playerheading'> Player " + i + " </h3><input class='form-control' type='text' name='player"+i+"Name' value='' placeholder='Enter Player Name'> <input class='form-control' type='text' name='player"+i+"Rollno' value='' placeholder='Enter Roll Number'><input class='form-control' type='email' name='player"+i+"Email' value='' placeholder='Enter Email'>\n  <input type='button' class='btn btn-secondary'  class='previousbtn' onclick='whenClickprevious()' ; value='Previous'>\n <input type='submit' class='btn btnnext' value='Submit'>";
+      last.style.display = "block";
+    }
+} 
+function whenClickprevious()
+{
+    var curr = document.getElementById(`page${j}`);
+    curr.style.display = "none";
+    j -= 1;
+    if( j > 0 )
+    {
+      var previous = document.getElementById(`page${j}`);
+      previous.style.display = "block";
+    }
+} 
