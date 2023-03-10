8d25f6993e46a90b2a82b643eb283dea7c2d5f29
SD-IITKGP <jaihindsourabh@kgpian.iitkgp.ac.in>
2023-03-11 00:28:38 +0530

    .

--- server.js
+++ server.js
@@ -185,6 +185,10 @@ app.get("/mail2/:token", function (req, res) {
   const Transporter = nodemailer.createTransport({
     host: "smtp.hostinger.com",
     port: 465,
+    secureConnection: false,
+    tls: {
+      rejectUnauthorized: false
+    },
     auth: {
       user: process.env.GMAILID,
       pass: process.env.GMAILAPPPASSWORD,
@@ -234,6 +238,10 @@ app.get("/mail", async function (req, res) {
   const Transporter = nodemailer.createTransport({
     host: "smtp.hostinger.com",
     port: 465,
+    secureConnection: false,
+    tls: {
+      rejectUnauthorized: false
+    },
     auth: {
     user: process.env.GMAILID,
     pass: process.env.GMAILAPPPASSWORD
