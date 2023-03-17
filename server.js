4fdcc1aa5e73b887759fb9ab828905e0ca006fb0
Rcaiitkgp <86818556+Rcaiitkgp@users.noreply.github.com>
2023-03-17 13:40:00 +0530

    Server Closed for coupon

--- server.js
+++ server.js
@@ -95,191 +95,191 @@ const User = mongoose.model("User", userSchema);
 app.get("/",function(req,res){
     res.send("I am On");
 });
-app.post("/razorpay", async function (req, res) {
-    if(req.body.number_of_coupons<=0){
-        res.redirect(process.env.WEBSITE_URL + "/failure.html");
-    }
-  console.log("IN");
-  const uid = new shortId();
-  const token = uid.stamp(32);
-  let cost = process.env.COUPON_PRICE * req.body.number_of_coupons;
-  var instance = new Razorpay({
-    key_id: process.env.RAZORPAY_KEY_ID,
-    key_secret: process.env.RAZORPAY_SECRET_KEY,
-  });
+// app.post("/razorpay", async function (req, res) {
+//     if(req.body.number_of_coupons<=0){
+//         res.redirect(process.env.WEBSITE_URL + "/failure.html");
+//     }
+//   console.log("IN");
+//   const uid = new shortId();
+//   const token = uid.stamp(32);
+//   let cost = process.env.COUPON_PRICE * req.body.number_of_coupons;
+//   var instance = new Razorpay({
+//     key_id: process.env.RAZORPAY_KEY_ID,
+//     key_secret: process.env.RAZORPAY_SECRET_KEY,
+//   });
 
-  instance.paymentLink
-    .create({
-      amount: cost * 100,
-      currency: "INR",
-      description: req.body.description,
-      customer: {
-        name: req.body.FirstName+" "+req.body.LastName,
-        email: req.body.email,
-      },
-      notes: {
-        contact: req.body.contact,
-        number_of_coupons: req.body.number_of_coupons,
-        roll_number: req.body.rollNumber,
-        hall: req.body.browser,
-        contact: req.body.contact,
-        roomNumber: req.body.room,
-        token: token,
-      },
-      // notify: {
-      //   email: true,
-      // },
-      options: {
-        prefill: {
-          contact: req.body.contact,
-          email: req.body.email,
-        },
-      },
-      callback_url: process.env.SELF_URL + "/success/razorpay?token=" + token,
-      callback_method: "get",
-    })
-    .then((response) => {
-      const user = new User({
-        token: token,
-        email: req.body.email,
-        firstName:req.body.FirstName,
-        lastName:req.body.LastName,
-        contact: req.body.contact,
-        id: response.id,
-        status: false,
-        coupons: req.body.number_of_coupons,
-        rollNumber: req.body.rollNumber,
-        hall: req.body.browser,
-        roomNumber: req.body.room,
-        coupons_used: 0,
-        token: token,
-        short_url: response.short_url,
-        id2: md5(response.id)
-      });
-      user.save();
-      console.log("Updated!!");
-      res.redirect("/mail2/" + token + "?email=" + req.body.email);
-    })
-    .catch((err) => {
-      console.log(err);
-      res.redirect(process.env.WEBSITE_URL + "/failure.html");
-    });
-});
+//   instance.paymentLink
+//     .create({
+//       amount: cost * 100,
+//       currency: "INR",
+//       description: req.body.description,
+//       customer: {
+//         name: req.body.FirstName+" "+req.body.LastName,
+//         email: req.body.email,
+//       },
+//       notes: {
+//         contact: req.body.contact,
+//         number_of_coupons: req.body.number_of_coupons,
+//         roll_number: req.body.rollNumber,
+//         hall: req.body.browser,
+//         contact: req.body.contact,
+//         roomNumber: req.body.room,
+//         token: token,
+//       },
+//       // notify: {
+//       //   email: true,
+//       // },
+//       options: {
+//         prefill: {
+//           contact: req.body.contact,
+//           email: req.body.email,
+//         },
+//       },
+//       callback_url: process.env.SELF_URL + "/success/razorpay?token=" + token,
+//       callback_method: "get",
+//     })
+//     .then((response) => {
+//       const user = new User({
+//         token: token,
+//         email: req.body.email,
+//         firstName:req.body.FirstName,
+//         lastName:req.body.LastName,
+//         contact: req.body.contact,
+//         id: response.id,
+//         status: false,
+//         coupons: req.body.number_of_coupons,
+//         rollNumber: req.body.rollNumber,
+//         hall: req.body.browser,
+//         roomNumber: req.body.room,
+//         coupons_used: 0,
+//         token: token,
+//         short_url: response.short_url,
+//         id2: md5(response.id)
+//       });
+//       user.save();
+//       console.log("Updated!!");
+//       res.redirect("/mail2/" + token + "?email=" + req.body.email);
+//     })
+//     .catch((err) => {
+//       console.log(err);
+//       res.redirect(process.env.WEBSITE_URL + "/failure.html");
+//     });
+// });
 
-app.get("/verify/:token", async function (req, res) {
-  const found = await User.find({ token: req.params.token }).exec();
-  console.log(found);
-  if (found.length != 0) {
-    res.redirect(found[0].short_url);
-  } else {
-    res.redirect(process.env.WEBSITE_URL + "/failure.html");
-  }
-});
+// app.get("/verify/:token", async function (req, res) {
+//   const found = await User.find({ token: req.params.token }).exec();
+//   console.log(found);
+//   if (found.length != 0) {
+//     res.redirect(found[0].short_url);
+//   } else {
+//     res.redirect(process.env.WEBSITE_URL + "/failure.html");
+//   }
+// });
 
-app.get("/success/razorpay", async function (req, res) {
-  const token = req.query.token;
-  const docs = await User.findOneAndUpdate(
-    { token: token },
-    { status: true },
-    {
-      new: true,
-    }
-  );
-  const email = docs.email;
-  res.redirect("/mail?email=" + email + "&id=" + docs.id2+"&passes="+docs.coupons);
-});
+// app.get("/success/razorpay", async function (req, res) {
+//   const token = req.query.token;
+//   const docs = await User.findOneAndUpdate(
+//     { token: token },
+//     { status: true },
+//     {
+//       new: true,
+//     }
+//   );
+//   const email = docs.email;
+//   res.redirect("/mail?email=" + email + "&id=" + docs.id2+"&passes="+docs.coupons);
+// });
 
 //Sending mails using nodemailer
-app.get("/mail2/:token", function (req, res) {
-  const Transporter = nodemailer.createTransport({
-    host: "smtp.hostinger.com",
-    port: 465,
-    secureConnection: false,
-    tls: {
-      rejectUnauthorized: false
-    },
-    auth: {
-      user: process.env.GMAILID,
-      pass: process.env.GMAILAPPPASSWORD,
-    },
-    from: process.env.GMAILID,
-  });
-  const data = "<p>Thanks for submitting your information for booking your pass of Khamma Ghani - The FoodFest 2023. </p> <br>Please click the link below to verify your email address and complete the booking of your pass by paying the applicable fees: <br>"+'<a href="' +
-  process.env.SELF_URL +
-  "/verify/" +
-  req.params.token +
-  '">' +
-  process.env.SELF_URL +
-  '/verify/' +
-  req.params.token +
-  '</a>'+"<p> In case the link doesn’t work, you are advised to re-register through the registration page-"+'<a href="' +
-  process.env.WEBSITE_URL +
-  "/KhammaGhani.html"+
-  '">' +
-  process.env.WEBSITE_URL +
-  "/KhammaGhani" +'</a>'+
-  "</p><p>Feel free to "+'<a href="'+process.env.WEBSITE_URL +
-  "/contact.html"
-  +'">' +
-  "reach out" +'</a>' +" to us in case of payment, registration or passes issues. <br><br>Thanks <br>Team RCA IIT Kharagpur</p>";
-  const mailOptions = {
-    from: process.env.GMAILID,
-    to: req.query.email,
-    subject: "Verify your email Address!!",
-    text: "Please verify your email!!",
-    html:data
-  };
+// app.get("/mail2/:token", function (req, res) {
+//   const Transporter = nodemailer.createTransport({
+//     host: "smtp.hostinger.com",
+//     port: 465,
+//     secureConnection: false,
+//     tls: {
+//       rejectUnauthorized: false
+//     },
+//     auth: {
+//       user: process.env.GMAILID,
+//       pass: process.env.GMAILAPPPASSWORD,
+//     },
+//     from: process.env.GMAILID,
+//   });
+//   const data = "<p>Thanks for submitting your information for booking your pass of Khamma Ghani - The FoodFest 2023. </p> <br>Please click the link below to verify your email address and complete the booking of your pass by paying the applicable fees: <br>"+'<a href="' +
+//   process.env.SELF_URL +
+//   "/verify/" +
+//   req.params.token +
+//   '">' +
+//   process.env.SELF_URL +
+//   '/verify/' +
+//   req.params.token +
+//   '</a>'+"<p> In case the link doesn’t work, you are advised to re-register through the registration page-"+'<a href="' +
+//   process.env.WEBSITE_URL +
+//   "/KhammaGhani.html"+
+//   '">' +
+//   process.env.WEBSITE_URL +
+//   "/KhammaGhani" +'</a>'+
+//   "</p><p>Feel free to "+'<a href="'+process.env.WEBSITE_URL +
+//   "/contact.html"
+//   +'">' +
+//   "reach out" +'</a>' +" to us in case of payment, registration or passes issues. <br><br>Thanks <br>Team RCA IIT Kharagpur</p>";
+//   const mailOptions = {
+//     from: process.env.GMAILID,
+//     to: req.query.email,
+//     subject: "Verify your email Address!!",
+//     text: "Please verify your email!!",
+//     html:data
+//   };
 
-  Transporter.sendMail(mailOptions)
-    .then((response) => {
-      res.redirect(process.env.WEBSITE_URL + "/registrationsuccessful.html");
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-});
+//   Transporter.sendMail(mailOptions)
+//     .then((response) => {
+//       res.redirect(process.env.WEBSITE_URL + "/registrationsuccessful.html");
+//     })
+//     .catch((err) => {
+//       console.log(err);
+//     });
+// });
 
-app.get("/mail", async function (req, res) {
-  console.log(req.query.id);
-  const id = req.query.id;
-  let img = await QRCode.toDataURL(id);
+// app.get("/mail", async function (req, res) {
+//   console.log(req.query.id);
+//   const id = req.query.id;
+//   let img = await QRCode.toDataURL(id);
 
-  const Transporter = nodemailer.createTransport({
-    host: "smtp.hostinger.com",
-    port: 465,
-    secureConnection: false,
-    tls: {
-      rejectUnauthorized: false
-    },
-    auth: {
-    user: process.env.GMAILID,
-    pass: process.env.GMAILAPPPASSWORD
-    },
-    from: process.env.GMAILID,
-  });
-  const data = "<p><b>Congratulations!</b><br> You have successfully booked your pass/passes for Khamma Ghani - The Foodfest 2023. </p><p>Please find your pass in the attachment, a unique QR Code. </p><p>The pass is your unique QR Code which you need to bring at the time of the event for checking before entry to the venue. In case you have registered more than one pass for your family/friends at a time, you are advised to come altogether at the venue for smooth verification process. </p><p>Please note:</p><p>Number of Passes Booked: "+req.query.passes+" <br><b>Date of Event: 17th March 2023<br></b></p><p>Looking forward to your presence in the event. <br>Thanks for registering for Khamma Ghani</p><b>Team RCA IIT Kharagpur</b>"
-  let mailOptions = {
-    from: process.env.GMAILID, // sender address
-    to: req.query.email, // list of receivers
-    subject: "Team, RCA IITKGP", // Subject line
-    // text: "Payment Successful", // plain text body
-    attachDataUrls: true,
-    html: data, // html body
-    attachments:[
-        {
-            path: img
-        }
-    ]
-  };
+//   const Transporter = nodemailer.createTransport({
+//     host: "smtp.hostinger.com",
+//     port: 465,
+//     secureConnection: false,
+//     tls: {
+//       rejectUnauthorized: false
+//     },
+//     auth: {
+//     user: process.env.GMAILID,
+//     pass: process.env.GMAILAPPPASSWORD
+//     },
+//     from: process.env.GMAILID,
+//   });
+//   const data = "<p><b>Congratulations!</b><br> You have successfully booked your pass/passes for Khamma Ghani - The Foodfest 2023. </p><p>Please find your pass in the attachment, a unique QR Code. </p><p>The pass is your unique QR Code which you need to bring at the time of the event for checking before entry to the venue. In case you have registered more than one pass for your family/friends at a time, you are advised to come altogether at the venue for smooth verification process. </p><p>Please note:</p><p>Number of Passes Booked: "+req.query.passes+" <br><b>Date of Event: 17th March 2023<br></b></p><p>Looking forward to your presence in the event. <br>Thanks for registering for Khamma Ghani</p><b>Team RCA IIT Kharagpur</b>"
+//   let mailOptions = {
+//     from: process.env.GMAILID, // sender address
+//     to: req.query.email, // list of receivers
+//     subject: "Team, RCA IITKGP", // Subject line
+//     // text: "Payment Successful", // plain text body
+//     attachDataUrls: true,
+//     html: data, // html body
+//     attachments:[
+//         {
+//             path: img
+//         }
+//     ]
+//   };
 
-  Transporter.sendMail(mailOptions, (error, info) => {
-    if (error) {
-      return console.log(error);
-    }
-    //console.log('Message %s sent: %s', info.messageId, info.response);
-    res.redirect(process.env.WEBSITE_URL + "/payment.html?qrcode="+img);
-  });
-});
+//   Transporter.sendMail(mailOptions, (error, info) => {
+//     if (error) {
+//       return console.log(error);
+//     }
+//     //console.log('Message %s sent: %s', info.messageId, info.response);
+//     res.redirect(process.env.WEBSITE_URL + "/payment.html?qrcode="+img);
+//   });
+// });
 //Admin Part
 
 app.get("/check/:paymentLink/:password", async function (req, res) {
